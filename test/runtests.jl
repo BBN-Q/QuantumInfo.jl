21f63dc18a179d6cc10153d939bd77386a8301c2
John Lapeyre <jlapeyre@users.noreply.github.com>
2018-11-20 23:52:40 +0100

    Upgrade runtests.jl for Julia v1.0
    	modified:   test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,11 @@ using Test
 import LinearAlgebra
 import QuantumInfo.eye
 
-rtoldefault = Base.rtoldefault
-
 #function isapprox(m1::Matrix,m2::Matrix; rtol::Real=rtoldefault(abs(m1[1,1]),abs(m2[1,1])), atol::Real=0)
 #  all(x->isapprox(x,0.0,rtol=rtol,atol=atol),abs(m1-m2))
 #end
 
-function Base.isapprox(v1::Vector, v2::Vector; atol::Real=0, rtol::Real=rtoldefault(abs(v1[1][1,1]),abs(v2[1][1,1]), atol))
+function Base.isapprox(v1::Vector, v2::Vector; atol::Real=0, rtol::Real=Base.rtoldefault(abs(v1[1][1,1]),abs(v2[1][1,1]), atol))
   local v
   if length(v1) != length(v2)
     false
@@ -32,38 +30,46 @@ end
 #  end
 #end
 
-let
+@testset "partial trace" begin
+    for i=1:100
+        da = round(Int,round(rand()*10))+1
+        db = round(Int,round(rand()*10))+1
+        ra = partialtrace(projector(randn(da*3)+1im*randn(da*3)),[da,3],2)
+        rb = partialtrace(projector(randn(db*3)+1im*randn(db*3)),[db,3],2)
+        @test isapprox(partialtrace(kron(ra,rb),[da,db],1),rb,atol=1e-15)
+        @test isapprox(partialtrace(kron(ra,rb),[da,db],2),ra,atol=1e-15)
+    end
+end
 
-  for i=1:100
-    da = round(Int,round(rand()*10))+1
-    db = round(Int,round(rand()*10))+1
-    ra = partialtrace(projector(randn(da*3)+1im*randn(da*3)),[da,3],2)
-    rb = partialtrace(projector(randn(db*3)+1im*randn(db*3)),[db,3],2)
-    @test isapprox(partialtrace(kron(ra,rb),[da,db],1),rb,atol=1e-15)
-    @test isapprox(partialtrace(kron(ra,rb),[da,db],2),ra,atol=1e-15)
-  end
+@testset "representation conversion" begin
+    id_choi = 1/2 .* [1 0 0 1;0 0 0 0;0 0 0 0;1 0 0 1];
 
-  id_choi = 1/2 .* [1 0 0 1;0 0 0 0;0 0 0 0;1 0 0 1];
+    @test liou(eye(2),eye(2)) == eye(4)
+    @test liou(eye(2)) == liou(eye(2),eye(2))
+    @test mat(vec(eye(2))) == eye(2)
+    @test mat([1,2,3,4]) == [1 3;2 4]
+    @test vec([1 2; 3 4]) == [1,3,2,4]
 
-  @test liou(eye(2),eye(2)) == eye(4)
-  @test liou(eye(2)) == liou(eye(2),eye(2))
-  @test mat(vec(eye(2))) == eye(2)
-  @test mat([1,2,3,4]) == [1 3;2 4]
-  @test vec([1 2; 3 4]) == [1,3,2,4]
+    @test choi_liou_involution(choi_liou_involution(eye(4)))==eye(4)
 
-  @test choi_liou_involution(choi_liou_involution(eye(4)))==eye(4)
+    @test isapprox(choi2liou(id_choi),eye(4))
+    @test isapprox(liou2choi(eye(4)),id_choi)
+    @test isapprox(choi2kraus(id_choi),Matrix{ComplexF64}[zeros(2,2), zeros(2,2), zeros(2,2), eye(2)],atol=1e-7)
+    @test isapprox(kraus2liou(Matrix{ComplexF64}[eye(2)]),eye(4))
+    @test isapprox(liou2kraus(eye(4)),Matrix{ComplexF64}[zeros(2,2), zeros(2,2), zeros(2,2), eye(2)],atol=1e-7)
+    @test isapprox(kraus2choi(Matrix{ComplexF64}[eye(2)]),id_choi)
 
-  @test isapprox(choi2liou(id_choi),eye(4))
-  @test isapprox(liou2choi(eye(4)),id_choi)
-  @test isapprox(choi2kraus(id_choi),Matrix{ComplexF64}[zeros(2,2), zeros(2,2), zeros(2,2), eye(2)],atol=1e-7)
-  @test isapprox(kraus2liou(Matrix{ComplexF64}[eye(2)]),eye(4))
-  @test isapprox(liou2kraus(eye(4)),Matrix{ComplexF64}[zeros(2,2), zeros(2,2), zeros(2,2), eye(2)],atol=1e-7)
-  @test isapprox(kraus2choi(Matrix{ComplexF64}[eye(2)]),id_choi)
+    @test isapprox(liou2pauliliou(eye(4)), eye(4))
+    @test isapprox(pauliliou2liou(eye(4)), eye(4))
+end
 
-  @test isapprox(depol(2),projector(vec(eye(2))),atol=1e-15)
-  @test isapprox(depol(2),depol(2,1.))
-  @test isapprox(depol(2,0.),eye(4),atol=1e-15)
+@testset "depolarization" begin
+    @test isapprox(depol(2),projector(vec(eye(2))),atol=1e-15)
+    @test isapprox(depol(2),depol(2,1.))
+    @test isapprox(depol(2,0.),eye(4),atol=1e-15)
+end
 
+@testset "density matrix properties" begin
   for i=1:100
     rrho = partialtrace(projector(randn(3^3)+1im*randn(3^3)),[3,9],2)
     @test isapprox(LinearAlgebra.tr(rrho),1.)
@@ -79,8 +85,4 @@ let
     @test ispossemidef(liou2choi(re),tol=1e-13)
     @test isapprox(partialtrace(liou2choi(re),[3,3],2),eye(3)/3,atol=1e-15)
   end
-
-  @test isapprox(liou2pauliliou(eye(4)), eye(4))
-  @test isapprox(pauliliou2liou(eye(4)), eye(4))
-
 end
